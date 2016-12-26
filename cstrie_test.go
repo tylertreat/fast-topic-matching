fdabad4f32807ae0538d7acca258194f1c6afead
Tyler Treat <ttreat31@gmail.com>
2016-12-25 21:00:01 -0600

    Add cstrie

---
+++ cstrie_test.go
@@ -0,0 +1,90 @@
+package matching
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCSTrieMatcher(t *testing.T) {
+	assert := assert.New(t)
+	var (
+		m  = NewCSTrieMatcher()
+		s0 = 0
+		s1 = 1
+		s2 = 2
+	)
+
+	sub0, err := m.Subscribe("forex.*", s0)
+	assert.NoError(err)
+	sub1, err := m.Subscribe("*.usd", s0)
+	assert.NoError(err)
+	sub2, err := m.Subscribe("forex.eur", s0)
+	assert.NoError(err)
+	sub3, err := m.Subscribe("*.eur", s1)
+	assert.NoError(err)
+	sub4, err := m.Subscribe("forex.*", s1)
+	assert.NoError(err)
+	sub5, err := m.Subscribe("trade", s1)
+	assert.NoError(err)
+	sub6, err := m.Subscribe("*", s2)
+	assert.NoError(err)
+
+	assertEqual(assert, []Subscriber{s0, s1}, m.Lookup("forex.eur"))
+	assertEqual(assert, []Subscriber{s2}, m.Lookup("forex"))
+	assertEqual(assert, []Subscriber{}, m.Lookup("trade.jpy"))
+	assertEqual(assert, []Subscriber{s0, s1}, m.Lookup("forex.jpy"))
+	assertEqual(assert, []Subscriber{s1, s2}, m.Lookup("trade"))
+
+	m.Unsubscribe(sub0)
+	m.Unsubscribe(sub1)
+	m.Unsubscribe(sub2)
+	m.Unsubscribe(sub3)
+	m.Unsubscribe(sub4)
+	m.Unsubscribe(sub5)
+	m.Unsubscribe(sub6)
+
+	assertEqual(assert, []Subscriber{}, m.Lookup("forex.eur"))
+	assertEqual(assert, []Subscriber{}, m.Lookup("forex"))
+	assertEqual(assert, []Subscriber{}, m.Lookup("trade.jpy"))
+	assertEqual(assert, []Subscriber{}, m.Lookup("forex.jpy"))
+	assertEqual(assert, []Subscriber{}, m.Lookup("trade"))
+}
+
+func BenchmarkCSTrieMatcherSubscribe(b *testing.B) {
+	var (
+		m  = NewCSTrieMatcher()
+		s0 = 0
+	)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.Subscribe("*.usd", s0)
+	}
+}
+
+func BenchmarkCSTrieMatcherUnsubscribe(b *testing.B) {
+	var (
+		m  = NewCSTrieMatcher()
+		s0 = 0
+	)
+	id, _ := m.Subscribe("*.usd", s0)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.Unsubscribe(id)
+	}
+}
+
+func BenchmarkCSTrieMatcherLookup(b *testing.B) {
+	var (
+		m  = NewCSTrieMatcher()
+		s0 = 0
+	)
+	m.Subscribe("*.usd", s0)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.Lookup("forex.usd")
+	}
+}
